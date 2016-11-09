f77b3a3022505cd3c27722dd025fa4b27eabd25f
Miftah Afina <miftah.afina@gmail.com>
2016-11-09 23:01:45 +0700

    Test remove

---
+++ content.js
@@ -0,0 +1,18 @@
+var word = 'trump';
+
+// Make it case insensitive
+$.expr[":"].contains = $.expr.createPseudo(function(arg) {
+    return function( elem ) {
+        return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+});
+
+// Search
+var hide = function() {
+  $(
+    "span:contains('"+ word +"'), a:contains('"+ word +"'), cite:contains('"+ word +"'), p:contains('"+ word +"')")
+    .css("display", "none");
+};
+
+// Run every 5 secs
+setInterval(hide, 5000);
